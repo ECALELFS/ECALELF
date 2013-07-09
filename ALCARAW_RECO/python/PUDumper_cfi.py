85c5e70a5c52494c8d45da84fa2fe1b12c664215
chiara la licata <chiara.la.licata@cern.ch>
2013-07-09 15:05:09 +0200

    moving stuff from Calibration to main directory because I don't know how to sparse checkout

--- ALCARAW_RECO/python/PUDumper_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-PUDumper = cms.EDAnalyzer('PUDumper',
-#                               vertexCollection = cms.InputTag('offlinePrimaryVertices'),
-#                               foutName = cms.string("ZShervinNtuple.root"),
-                          )
--- JsonFilter/python/jsonFilter_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-jsonFilter = cms.EDFilter("JsonFilter",
-                          jsonFileName = cms.string("")
-                          )
-
-
-
-
