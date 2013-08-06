7f119791dbcd869d4262c7ea5ce8342bb26f2cd8
Shervin <shervin@cern.ch>
2013-08-06 15:16:33 +0200

    pf deposit name as standard egamma ones produced on RECO

--- ValueMapTraslator/python/valuemaptraslator_cfi.py
+++ ValueMapTraslator/python/valuemaptraslator_cfi.py
@@ -1,19 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 elPFIsoValueCharged03PFIdRecalib = cms.EDProducer('ValueMapTraslator',
-                                                  inputCollection = cms.InputTag('elPFIsoValueCharged03PFIdGsf'),
+                                                  inputCollection = cms.InputTag('elPFIsoValueCharged03PFId'),
                                                   outputCollection = cms.string(''),
                                                   referenceCollection = cms.InputTag('electronRecalibSCAssociator')
                                                   )
 
 elPFIsoValueGamma03PFIdRecalib = cms.EDProducer('ValueMapTraslator',
-                                                inputCollection = cms.InputTag('elPFIsoValueGamma03PFIdGsf'),
+                                                inputCollection = cms.InputTag('elPFIsoValueGamma03PFId'),
                                                 outputCollection = cms.string(''),
                                                 referenceCollection = cms.InputTag('electronRecalibSCAssociator')
                                                 )
 
 elPFIsoValueNeutral03PFIdRecalib = cms.EDProducer('ValueMapTraslator',
-                                                inputCollection = cms.InputTag('elPFIsoValueNeutral03PFIdGsf'),
+                                                inputCollection = cms.InputTag('elPFIsoValueNeutral03PFId'),
                                                 outputCollection = cms.string(''),
                                                 referenceCollection = cms.InputTag('electronRecalibSCAssociator')
                                                 )
