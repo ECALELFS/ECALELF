558787df4c8563996a6530fbad4fa64bdf8df76f
Raffaele <raffaele.gerosa@cern.ch>
2013-12-11 11:11:39 -0600

    setup reading git repo

---
+++ setup_git.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+checkVERSION(){
+    case $CMSSW_VERSION in
+	CMSSW_5_3_7_patch6)
+	    echo "[INFO] Installing for $CMSSW_VERSION (2012 8TeV)"
+	    ;;
+	*)
+	    echo "[ERROR] Sorry, $CMSSW_VERSION not configured for ECALELF"
+	    echo "        Be sure that you don't want 4_2_8_patch7 or CMSSW_4_4_5_patch2 or 5_3_7_patch5 or CMSSW_6_1_*"
+	    
+	    exit 1
+	    ;;
+    esac
+}
+
+case $# in
+    1)
+	echo "[STATUS] Creating $1 CMSSW release working area"
+	CMSSW_VERSION=$1
+	checkVERSION 
+	scram project CMSSW ${CMSSW_VERSION}
+	cd ${CMSSW_VERSION}/src
+	eval `scramv1 runtime -sh`
+	;;
+    *)
+	checkVERSION
+	;;
+esac
+
+export CMSSW_VERSION
+
+
+# put in the right directory
+cd $CMSSW_BASE/src
+
+#########################################################################################
+echo "[STATUS] Download of the skims"
+# last WSkim version
+git-cms-addpkg DataFormats/EgammaCandidates
+git clone https://github.com/cms-cvs-history/DPGAnalysis-Skims DPGAnalysis/Skims
+cd DPGAnalysis/Skims/
+git checkout DPGAnalysis-Skims-V01-00-07
+cd -
+
+#########################################################################################
+echo "[STATUS] Patch GsfElectrons for ECALELF rereco"
+sed -i 's|[/]*assert|////assert|' DataFormats/EgammaCandidates/src/GsfElectron.cc 
+
+#########################################################################################
+echo "[STATUS] Download ECALELF directory"
+myDir=Calibration
+if [ ! -d "$myDir" ];then
+    git clone https://github.com/rgerosa/ECALELF $myDir #
+    #git clone git://github.com/ECALELFS/ECALELF.git Calibration # read-only mode
+fi
+cd $myDir
+
+cd ALCARAW_RECO/
+
+### if you are not Shervin download this to have some useful scripts
+if [ "$USER" != "shervin" ];then
+svn co svn+ssh://svn.cern.ch/reps/analysis/trunk/bin bin
+# Please be sure to add this directory to you default PATH variable
+# for bash
+#PATH=$PATH:$CMSSW_BASE/src/calibration/ALCARAW_RECO/bin
+# for tcsh
+#setenv PATH $PATH":$CMSSW_BASE/src//calibration/SANDBOX/bin" # (I think)
+###
+echo "[WARNING] Remember to do this after all cmsenv:"
+echo "# for bash
+PATH=$PATH:$CMSSW_BASE/src/$myDir/ALCARAW_RECO/bin
+# for tcsh
+setenv PATH $PATH\":$CMSSW_BASE/src/$myDir/ALCARAW_RECO/bin\" 
+"
+
+        cd EgammaAnalysis/ElectronTools/data/
+        cat download.url | xargs wget
+        cd -
+fi
+
+cd $CMSSW_BASE/src
+
+#Other package to download:
+# - Last stable pattuple code:
+case $CMSSW_VERSION in
+    CMSSW_5_*)
+	#addpkg  PhysicsTools/PatAlgos V08-10-05 >> setup.log
+###### - Electron regression 5_3_X
+	git clone https://github.com/cms-cvs-history/DataFormats-PatCandidates DataFormats/PatCandidates >> setup.log || exit 1
+        cd DataFormats/PatCandidates >> setup.log || exit 1
+        git checkout DataFormats-PatCandidates-V06-05-06-10 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+	git clone https://github.com/cms-cvs-history/PhysicsTools-PatAlgos PhysicsTools/PatAlgos  >> setup.log || exit 1
+        cd PhysicsTools/PatAlgos >> setup.log || exit 1
+        git checkout PhysicsTools-PatAlgos-V08-09-56 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+	git clone https://github.com/cms-cvs-history/PhysicsTools-PatUtils PhysicsTools/PatUtils  >> setup.log || exit 1
+        cd PhysicsTools/PatUtils >> setup.log || exit 1
+        git checkout PhysicsTools-PatUtils-V03-09-28 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+	git clone https://github.com/cms-cvs-history/DataFormats-CaloRecHit DataFormats/CaloRecHit  >> setup.log || exit 1
+        cd DataFormats/CaloRecHit >> setup.log || exit 1
+        git checkout DataFormats-CaloRecHit-V02-05-11 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+	git clone https://github.com/cms-cvs-history/DataFormats-StdDictionaries DataFormats/StdDictionaries  >> setup.log || exit 1
+        cd DataFormats/StdDictionaries >> setup.log || exit 1
+        git checkout DataFormats-StdDictionaries-V00-02-14 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+	git clone https://github.com/cms-cvs-history/FWCore-GuiBrowsers FWCore/GuiBrowsers  >> setup.log || exit 1
+        cd FWCore/GuiBrowsers >> setup.log || exit 1
+        git checkout FWCore-GuiBrowsers-V00-00-70 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+	git clone https://github.com/cms-cvs-history/RecoMET-METProducers RecoMET/METProducers  >> setup.log || exit 1
+        cd RecoMET/METProducers >> setup.log || exit 1
+        git checkout RecoMET-METProducers-V03-03-12-02 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+	git clone https://github.com/cms-cvs-history/RecoEgamma-EgammaTools RecoEgamma/EgammaTools  >> setup.log || exit 1
+        cd RecoEgamma/EgammaTools >> setup.log || exit 1
+        git checkout RecoEgamma-EgammaTools-V09-00-01 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+        git clone https://github.com/cms-analysis/EgammaAnalysis-ElectronTools EgammaAnalysis/ElectronTools >> setup.log || exit 1
+        cd EgammaAnalysis/ElectronTools  >> setup.log || exit 1
+	git checkout EgammaAnalysis-ElectronTools-MD-21Apr2013-test-2 >> setup.log || exit 1
+        cd - >> setup.log || exit 1
+	patch -p0 < $myDir/ALCARAW_RECO/test/electronRegression.patch >> setup.log || exit 1
+
+	cd EgammaAnalysis/ElectronTools/data/ >> setup.log || exit 1
+	cat download.url | xargs wget  >> setup.log || exit 1
+	cd - >> setup.log || exit 1
+###### - Regression from Josh 5_3_X
+	#checkdeps -a
+
+	echo "[STATUS] applying patch for CMSSW_5_X"
+	sed 's|,eleIt->ecalEnergyError()\*(nearestSC.*);|);|' $myDir/ALCARAW_RECO/src/ElectronRecalibSuperClusterAssociatorSH.cc_topatch > $myDir/ALCARAW_RECO/src/ElectronRecalibSuperClusterAssociatorSH.cc
+
+
+    if [ ! -e "$CMSSW_BASE/src/RecoEcal/EgammaCoreTools" ];then
+        git-cms-addpkg RecoEcal/EgammaCoreTools >> setup.log || exit 1
+    fi
+    if [ "`grep -c getEcalEBRecHitCollection $CMSSW_BASE/src/RecoEcal/EgammaCoreTools/interface/EcalClusterLazyTools.h`" == "0" ];then
+	patch  -p0 < $myDir/ALCARAW_RECO/test/clusterLazyTools.patch >> setup.log || exit 1
+    fi
+    patch  -p0 < $myDir/ALCARAW_RECO/test/class_def.xml.patch >> setup.log || exit 1
+
+	cp /afs/cern.ch/user/b/bendavid/cmspublic/regweights52xV3/*.root $myDir/EleNewEnergiesProducer/data/ >> setup.log || exit 1
+
+
+	;;
+esac
+
+
+
+# compile
+scram b -j8
+
+for file in `find -name '*.url'`; 
+  do 
+  if [ "`basename $file`" == "copy.url" ];then 
+      #echo $file; 
+      for cpFile in `cat $file`; 
+	do 
+	cp -u $cpFile `dirname $file`; 
+      done; 
+  fi; 
+  if [ "`basename $file`" == "download.url" ];then 
+      echo $file; 
+      for cpFile in `cat $file`; 
+	do 
+	cvs co -l -d `dirname $file` $cpFile
+      done; 
+  fi; 
+  
+done
+
+####-------->  Each time you set your environment!
+#PATH=$PATH:$CMSSW_BASE/$myDir/ALCARAW_RECO/bin
+#Previous command MUST be executed every time you log in 
+
