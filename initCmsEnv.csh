159591221ca4775621eaf7d3185352e8b1e6051e
Shervin <shervin@cern.ch>
2014-01-22 16:27:48 +0100

    added shell variable init scripts

---
+++ initCmsEnv.csh
@@ -0,0 +1,12 @@
+#!/bin/tcsh
+
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.csh
+cmsenv
+source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.csh
+voms-proxy-init -voms cms -out $HOME/gpi.out
+
+#cd calibration/SANDBOX
+#setenv PATH $PWD/bin:$PATH
+setenv PATH $PATH":/afs/cern.ch/project/eos/installation/pro/bin/"
+setenv PATH $PATH":$CMSSW_BASE/Calibration/ALCARAW_RECO/bin"
+
---
+++ initCmsEnv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
+cmsenv
+source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh
+voms-proxy-init -voms cms -out $HOME/gpi.out
+
+#cd calibration/SANDBOX
+#setenv PATH $PWD/bin:$PATH
+#PATH=`echo $PATH | sed 's|/afs/cern.ch/project/eos/installation/pro/bin/||'`
+
+PATH=$PATH:/afs/cern.ch/project/eos/installation/pro/bin/
+PATH=$PATH:$CMSSW_BASE/Calibration/ALCARAW_RECO/bin
+
