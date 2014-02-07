61874746140ccd37da52688bfe3a2ae4ca30f884
Margherita <margherita.obertino@cern.ch>
2014-02-07 17:41:38 +0100

    testing

--- ALCARAW_RECO/python/alcaSkimming.py
+++ ALCARAW_RECO/python/alcaSkimming.py
@@ -422,7 +422,7 @@ else:
 if(re.match("CMSSW_6_.*", CMSSW_VERSION)):
     process.alcarerecoSeq=cms.Sequence( process.trivialCond * process.sandboxPFRerecoSeq * (process.seqALCARECOEcalCalElectronRECO + process.reducedEcalRecHitsES))
 else:
-    process.alcarerecoSeq=cms.Sequence( process.trivialCond * process.sandboxRerecoSeq * (process.seqALCARECOEcalCalElectronRECO + process.reducedEcalRecHitsES))
+    process.alcarerecoSeq=cms.Sequence( process.jsonFilter * process.trivialCond * process.sandboxRerecoSeq * (process.seqALCARECOEcalCalElectronRECO + process.reducedEcalRecHitsES))
 
 
 process.rhoFastJetSeq = cms.Sequence()
