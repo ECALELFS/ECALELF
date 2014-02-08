62c32cd544f41fc9b03121201f995b9d2f02601a
Shervin <shervin@cern.ch>
2014-02-08 12:49:51 +0100

    updated documentation

--- mkDoc.sh
+++ mkDoc.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -d "doc/doxygen/fulldoc/" ];then
+    mkdir -p doc/doxygen/fulldoc/
+fi
 if [ ! -d "doc/doxygen/fulldoc/html" ];then
     cd doc/doxygen/fulldoc/
     git clone -b gh-pages git@github.com:ECALELFS/ECALELF.git html
