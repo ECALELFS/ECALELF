f9a02e8a5a5788714ba114086f2b0bff32fd4e57
Shervin <shervin@cern.ch>
2014-07-14 19:44:55 +0200

    rereco to test IC and eta scale

---
+++ ALCARAW_RECO/config/reRecoTags/Test_July2014_ICRunD_v1.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+RerecoGlobalTag = cms.ESSource("PoolDBESSource",
+                               CondDBSetup,
+                               connect = cms.string('frontier://FrontierProd/CMS_COND_31X_GLOBALTAG'),
+                               globaltag = cms.string('FT_R_53_V21::All'), 
+                               toGet = cms.VPSet(
+    cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
+             tag = cms.string("EcalIntercalibConstants_V20140708_EtaScaleStudies"),
+             connect = cms.untracked.string("frontier://FrontierInt/CMS_COND_ECAL")
+             )
+    ),
+                               BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService')
+                               )
