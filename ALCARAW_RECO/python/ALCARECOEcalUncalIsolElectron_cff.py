d69c40c0f28ccc7aa8e5ee5cc1cfe10ce29104e4
lbrianza <lucabrianza@gmail.com>
2014-10-29 10:28:58 +0100

    new local reco for 72x

--- ALCARAW_RECO/python/ALCARECOEcalUncalIsolElectron_cff.py
+++ ALCARAW_RECO/python/ALCARECOEcalUncalIsolElectron_cff.py
@@ -1,3 +1,4 @@
+import os, re
 import FWCore.ParameterSet.Config as cms
 
 #restarting from ECAL RAW to reconstruct amplitudes and energies
@@ -5,7 +6,14 @@ import FWCore.ParameterSet.Config as cms
 from Calibration.ALCARAW_RECO.ALCARECOEcalCalIsolElectron_cff import *
 from Configuration.StandardSequences.RawToDigi_Data_cff import *
 from RecoLocalCalo.Configuration.RecoLocalCalo_cff import *
-ecalUncalibRecHitSequence = cms.Sequence(ecalGlobalUncalibRecHit * ecalDetIdToBeRecovered)
+
+CMSSW_VERSION=os.getenv("CMSSW_VERSION")
+
+if (re.match("CMSSW_7_2_X",CMSSW_VERSION)):
+    from RecoLocalCalo.EcalRecProducers.ecalMultiFitUncalibRecHit_cfi import *
+    ecalUncalibRecHitSequence = cms.Sequence(ecalMultiFitUncalibRecHit * ecalDetIdToBeRecovered)
+else:
+    ecalUncalibRecHitSequence = cms.Sequence(ecalGlobalUncalibRecHit * ecalDetIdToBeRecovered)
         
 uncalibRecHitSeq = cms.Sequence( (ecalDigis + ecalPreshowerDigis) * ecalUncalibRecHitSequence)
 
