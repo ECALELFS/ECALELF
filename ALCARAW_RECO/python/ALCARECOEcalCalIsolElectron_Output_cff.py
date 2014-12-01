3068b321b0cffcab3ecd7772f2f332af8a26e7c0
lbrianza <lucabrianza@gmail.com>
2014-12-01 18:21:52 +0100

    filter for caloClusters

--- ALCARAW_RECO/python/ALCARECOEcalCalIsolElectron_Output_cff.py
+++ ALCARAW_RECO/python/ALCARECOEcalCalIsolElectron_Output_cff.py
@@ -14,7 +14,10 @@ OutALCARECOEcalCalElectron_specific_ = cms.untracked.vstring(
     'drop *_*Unclean*_*_*',
     'drop *_*unclean*_*_*',
     'drop *_*_*Unclean*_*',
-    'drop *_*_*unclean*_*'
+    'drop *_*_*unclean*_*',
+    'drop *CaloCluster*_*particleFlowEGamma*_*EBEEClusters*_*',
+    'drop *CaloCluster*_*particleFlowEGamma*_*ESClusters*_*',
+    'keep *CaloCluster*_*alCaIsolatedElectrons*_*alcaCaloCluster*_*'
 )
 
 OutALCARECOEcalCalElectron_noDrop_ = cms.PSet(
