e8d7ff4e88dc52d0cbb2cfe72ba92d966ed8ea37
Shervin <shervin@cern.ch>
2015-03-15 18:56:38 +0100

    changes

--- EcalAlCaRecoProducers/python/ALCARECOEcalESAlign_cff.py
+++ EcalAlCaRecoProducers/python/ALCARECOEcalESAlign_cff.py
@@ -5,8 +5,18 @@ from Calibration.EcalAlCaRecoProducers.EcalAlCaESAlignTrackReducer_cfi import *
 
 # this imports the filter that skims the events requiring a min number of selected tracks
 
+esSelectedTracks = cms.EDFilter("TrackSelector",
+                                src = cms.InputTag('generalTracks'),
+                                cut = cms.string("abs(eta)>1.7 && abs(eta)<2.3 && pt>1 && numberOfValidHits>=10")
+                                )
+esMinTrackNumberFilter = cms.EDFilter("TrackCountFilter",
+                                  src = cms.InputTag('ecalAlCaESAlignTrackReducer:generalTracks'),
+                                  minNumber = cms.uint32(10)
+                                  )
+
 #EcalESAlignTracksSkimSeq = cms.Sequence()
-EcalESAlignTracksSkimSeq = cms.Sequence( ecalAlCaESAlignTrackReducer) 
+#ecalAlCaESAlignTrackReducer.generalTracksLabel = 'esSelectedTracks'
+EcalESAlignTracksSkimSeq = cms.Sequence( esSelectedTracks * ecalAlCaESAlignTrackReducer * esMinTrackNumberFilter) 
 # * EcalAlCaESAlignSkimFilter )
 
 
