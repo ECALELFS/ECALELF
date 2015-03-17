f168c5f85ddfdb1450068b4df2e429963c7f6ca9
Shervin <shervin@cern.ch>
2015-03-17 15:07:42 +0100

    Revert "adding track reducer to sequence"
    This reverts commit 0e2accff14fae776cb2453cec7e4277c4ea2713e.

--- EcalAlCaRecoProducers/python/ALCARECOEcalESAlign_cff.py
+++ EcalAlCaRecoProducers/python/ALCARECOEcalESAlign_cff.py
@@ -1,13 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 # this imports the module that produces a reduced collections for ES alignment
-from Calibration.EcalAlCaRecoProducers.EcalAlCaESAlignTrackReducer_cfi import *
+#from Calibration.EcalAlCaRecoProducers.EcalAlCaESAlignTrackReducer import *
 
 # this imports the filter that skims the events requiring a min number of selected tracks
 
-#EcalESAlignTracksSkimSeq = cms.Sequence()
-EcalESAlignTracksSkimSeq = cms.Sequence( ecalAlCaESAlignTrackReducer) 
-# * EcalAlCaESAlignSkimFilter )
+EcalESAlignTracksSkimSeq = cms.Sequence()
+#EcalESAlignTracksSkimSeq = cms.Sequence( EcalAlCaESAlignTrackReducer * EcalAlCaESAlignSkimFilter )
 
 
 seqEcalESAlign = cms.Sequence(EcalESAlignTracksSkimSeq)
