c7fe3b2cfcdd1bfc1cdef3872bc2c93a3dcb0314
lbrianza <lucabrianza@gmail.com>
2015-09-29 15:34:33 +0200

    rereco Run2015C_WF2_ICCustomisation2015

---
+++ EcalAlCaRecoProducers/config/reRecoTags/Run2015C_WF2_ICCustomisation2015.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+# rereco of cycle Run2015B_WF2
+# other infos
+
+RerecoGlobalTag = cms.ESSource("PoolDBESSource",
+                               CondDBSetup,
+                               connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                               globaltag = cms.string('74X_dataRun2_Prompt_withOfflineCustomisations_v0'),
+                               )
