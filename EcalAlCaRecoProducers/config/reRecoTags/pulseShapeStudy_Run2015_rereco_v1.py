7a31faaec2c7b104d6726939d62fd35b628b6fe2
Shervin <shervin@cern.ch>
2015-10-25 20:56:46 +0100

    validation of pulse shape tag with lone bunch

---
+++ EcalAlCaRecoProducers/config/reRecoTags/pulseShapeStudy_Run2015_rereco_v1.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+RerecoGlobalTag = cms.ESSource("PoolDBESSource",
+                               CondDBSetup,
+                               connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                               globaltag = cms.string('74X_dataRun2_Prompt_v2'),
+                               toGet = cms.VPSet(
+                                 cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
+                                          tag = cms.string("EcalIntercalibConstants_2012ABCD_offline"),
+                                          connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_ECAL"),
+                                          ),
+                                 cms.PSet(record = cms.string("EcalPulseShapesRcd"),
+                                          tag = cms.string("EcalPulseShapes_data"),
+                                          connect = cms.untracked.string("sqlite_file:/afs/cern.ch/work/e/emanuele/public/ecal/pulseshapes_db/ecaltemplates_popcon_data_Run2015D_AlCaP0_lonebunch.db"),
+                                          ),
+                                 cms.PSet(record = cms.string("EBAlignmentRcd"),
+                                          tag = cms.string("EBAlignment_measured_v10_offline"),
+                                          connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                                          ),
+                                 cms.PSet(record = cms.string("EEAlignmentRcd"),
+                                          tag = cms.string("EEAlignment_measured_v10_offline"),
+                                          connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                                          ),
+                                 cms.PSet(record = cms.string("ESAlignmentRcd"), # only Bon!
+                                          tag = cms.string("ESAlignment_measured_v08_offline"),
+                                          connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                                          ),
+                               )
+)
