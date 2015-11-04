012c904bfe5358a814fb746be94b4c899423c4d7
gfasanel <giuseppe.fasanella@cern.ch>
2015-11-04 13:32:37 +0100

    working on producing miniAOD

--- EcalAlCaRecoProducers/run_alcareco.sh
+++ EcalAlCaRecoProducers/run_alcareco.sh
@@ -1,1 +1,11 @@
-./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep MINIAOD`  -s ZSkim --scheduler=remoteGlidein -t config/reRecoTags/phiSym_materialCorrection_ref.py --type MINIAODNTUPLE
+#source ../initCmsEnv.sh
+#cmsenv
+./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep giuseppe` --skim ZSkim --type ALCARECO -t config/reRecoTags/phiSym_materialCorrection_ref.py
+
+#--jobname test_giuseppe
+
+#./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep MINIAOD`  -s ZSkim --scheduler=remoteGlidein -t config/reRecoTags/phiSym_materialCorrection_ref.py --createOnly
+#scheduler only for crab2
+#--isMC
+#./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep giuseppe`  -s ZSkim --scheduler=remoteGlidein -t config/reRecoTags/phiSym_materialCorrection_ref.py --createOnly
+#--type MINIAODNTUPLE
