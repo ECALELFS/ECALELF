44cd472fd48aa0dd13387db20e02f8f6f97302f7
gfasanel <giuseppe.fasanella@cern.ch>
2015-11-16 19:30:12 +0100

    eleID using _

--- ZNtupleDumper/interface/eleIDMap.h
+++ ZNtupleDumper/interface/eleIDMap.h
@@ -27,15 +27,15 @@ class eleIDMap {
     eleIDmap["tight50nsRun2"]     =0x1000;
 
     //official eleIDs
-    eleIDmap["cutBasedElectronID|Spring15|25ns|V1|standalone|veto"]   = 0x10000;
-    eleIDmap["cutBasedElectronID|Spring15|25ns|V1|standalone|loose"]  = 0x20000;
-    eleIDmap["cutBasedElectronID|Spring15|25ns|V1|standalone|medium"] = 0x40000;
-    eleIDmap["cutBasedElectronID|Spring15|25ns|V1|standalone|tight"]  = 0x80000;
+    eleIDmap["cutBasedElectronID_Spring15_25ns_V1_standalone_veto"]   = 0x10000;
+    eleIDmap["cutBasedElectronID_Spring15_25ns_V1_standalone_loose"]  = 0x20000;
+    eleIDmap["cutBasedElectronID_Spring15_25ns_V1_standalone_medium"] = 0x40000;
+    eleIDmap["cutBasedElectronID_Spring15_25ns_V1_standalone_tight"]  = 0x80000;
     
-    eleIDmap["cutBasedElectronID|Spring15|50ns|V1|standalone|veto"]   = 0x100000;
-    eleIDmap["cutBasedElectronID|Spring15|50ns|V1|standalone|loose"]  = 0x200000;
-    eleIDmap["cutBasedElectronID|Spring15|50ns|V1|standalone|medium"] = 0x400000;
-    eleIDmap["cutBasedElectronID|Spring15|50ns|V1|standalone|tight"]  = 0x800000;
+    eleIDmap["cutBasedElectronID_Spring15_50ns_V1_standalone_veto"]   = 0x100000;
+    eleIDmap["cutBasedElectronID_Spring15_50ns_V1_standalone_loose"]  = 0x200000;
+    eleIDmap["cutBasedElectronID_Spring15_50ns_V1_standalone_medium"] = 0x400000;
+    eleIDmap["cutBasedElectronID_Spring15_50ns_V1_standalone_tight"]  = 0x800000;
 
 //'cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-loose' 'cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-medium' 'cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-tight' 'cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-veto' 'cutBasedElectronID-Spring15-25ns-V1-standalone-loose' 'cutBasedElectronID-Spring15-25ns-V1-standalone-medium' 'cutBasedElectronID-Spring15-25ns-V1-standalone-tight' 'cutBasedElectronID-Spring15-25ns-V1-standalone-veto' 'cutBasedElectronID-Spring15-50ns-V1-standalone-loose' 'cutBasedElectronID-Spring15-50ns-V1-standalone-medium' 'cutBasedElectronID-Spring15-50ns-V1-standalone-tight' 'cutBasedElectronID-Spring15-50ns-V1-standalone-veto' 'eidLoose' 'eidRobustHighEnergy' 'eidRobustLoose' 'eidRobustTight' 'eidTight' 'heepElectronID-HEEPV60' 'mvaEleID-Spring15-25ns-nonTrig-V1-wp80' 'mvaEleID-Spring15-25ns-nonTrig-V1-wp90''cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-loose' 'cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-medium' 'cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-tight' 'cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-veto' 'cutBasedElectronID-Spring15-25ns-V1-standalone-loose' 'cutBasedElectronID-Spring15-25ns-V1-standalone-medium' 'cutBasedElectronID-Spring15-25ns-V1-standalone-tight' 'cutBasedElectronID-Spring15-25ns-V1-standalone-veto' 'cutBasedElectronID-Spring15-50ns-V1-standalone-loose' 'cutBasedElectronID-Spring15-50ns-V1-standalone-medium' 'cutBasedElectronID-Spring15-50ns-V1-standalone-tight' 'cutBasedElectronID-Spring15-50ns-V1-standalone-veto' 'eidLoose' 'eidRobustHighEnergy' 'eidRobustLoose' 'eidRobustTight' 'eidTight' 'heepElectronID-HEEPV60' 'mvaEleID-Spring15-25ns-nonTrig-V1-wp80' 'mvaEleID-Spring15-25ns-nonTrig-V1-wp90'
   }
