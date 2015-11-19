2b836a288802c10b6e8d89ed5b930bc1dd0bb9d9
gfasanel <giuseppe.fasanella@cern.ch>
2015-11-19 10:04:27 +0100

    run_alcareco.sh added

--- EcalAlCaRecoProducers/run_alcareco.sh
+++ EcalAlCaRecoProducers/run_alcareco.sh
@@ -1,11 +1,3 @@
-#source ../initCmsEnv.sh
-#cmsenv
-./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep giuseppe` --skim ZSkim --type ALCARECO -t config/reRecoTags/phiSym_materialCorrection_ref.py
+#./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep giuseppe` --skim ZSkim --type ALCARECO -t config/reRecoTags/phiSym_materialCorrection_ref.py
 
-#--jobname test_giuseppe
-
-#./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep MINIAOD`  -s ZSkim --scheduler=remoteGlidein -t config/reRecoTags/phiSym_materialCorrection_ref.py --createOnly
-#scheduler only for crab2
-#--isMC
-#./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep giuseppe`  -s ZSkim --scheduler=remoteGlidein -t config/reRecoTags/phiSym_materialCorrection_ref.py --createOnly
-#--type MINIAODNTUPLE
+./scripts/prodAlcareco.sh `parseDatasetFile.sh alcareco_datasets.dat | grep miniAOD` --skim ZSkim --type ALCARECO -t config/reRecoTags/phiSym_materialCorrection_ref.py --scheduler=lsf
