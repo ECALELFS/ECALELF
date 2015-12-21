fe709f12ab862dc11b4a7292d60dca2127ba0604
lbrianza <lucabrianza@gmail.com>
2015-12-21 12:45:27 +0100

    calibration momentum stuff

--- EOverPCalibration/cfg/calibrationMomentum_cfg.py
+++ EOverPCalibration/cfg/calibrationMomentum_cfg.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("calibrationMomentumparameters")
+process = cms.Process("CalibrationMomentum")
 
 process.Options = cms.PSet(
 
@@ -8,7 +8,7 @@ process.Options = cms.PSet(
     infileDATA = cms.string("cfg/listDATA_momentumCalibration.txt"),
     infileMC = cms.string("cfg/listMC2012_momentumCalibration.txt"),
     WeightforMC = cms.string("CommonTools/PUweights_DYJetsToLL_Summer12_Prompt_TrueNumInteractions.root"),
-    usePUweights = cms.bool(false),
+    usePUweights = cms.bool(False),
     nPhiBinsEB = cms.int32(360),
     nPhiBinsEE = cms.int32(360),
     nEtaBinsEB = cms.int32(1),
@@ -17,7 +17,9 @@ process.Options = cms.PSet(
     nPhiBinsTempEE = cms.int32(1),
     rebinEB = cms.int32(10),
     rebinEE = cms.int32(20),
-    outputFile = cms.string("output/MomentumCalibration2012.root")
+    outputFile = cms.string("output/MomentumCalibration2015.root"),
+    typeEB = cms.string("eta1"),
+    typeEE = cms.string("eta1")
 
 )
 
