92171acf9489e44f2494ef3ff8478b80b2b6ee0f
gfasanel <giuseppe.fasanella@cern.ch>
2016-01-28 16:58:03 +0100

    divide run ranges

--- ZFitter/script/divider_run.sh
+++ ZFitter/script/divider_run.sh
@@ -3,6 +3,7 @@
 #2 the interval
 file=$(echo $1 |sed "s|data/validation/||"| sed "s|.dat||" ) #so you can tab the name of the file :-)
 interval=$2
+#data/runRanges/runRangeLimits.dat is used
 ./bin/ZFitter.exe -f data/validation/$file.dat --runDivide --nEvents_runDivide $interval >tmp/runranges.txt
 #cat tmp/runranges.txt | grep "-" | grep -v "DEBUG" | grep -v "pileup" | grep -v "R9Eleprime" | grep -v "selected" #show at screen
 cat tmp/runranges.txt | grep "-" | grep -v "DEBUG" | grep -v "pileup" | grep -v "R9Eleprime" | grep -v "selected"> data/runRanges/${file}_interval_${interval}.dat
