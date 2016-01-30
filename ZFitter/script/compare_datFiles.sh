9f8ed726eed4fb88aad4204b932a2a51858f8004
gfasanel <giuseppe.fasanella@cern.ch>
2016-01-30 16:17:38 +0100

    latex table writer from fit results

--- ZFitter/script/compare_datFiles.sh
+++ ZFitter/script/compare_datFiles.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
-
-commonCut=-Et_25-noPF
+commonCut=-Et_20-noPF
 echo "commonCut for the nominal file is $commonCut"
-nominal_file=$1
-syst_files="${@:2}"
 
-##################Function definition#################################################################################################
+for file in "$@"
+do
+    ./script/latex_table_writer.sh $file $commonCut
+done
+
+nominal_file=tmp/`basename $1 .dat`_smear.dat
+for file in "${@:2}"
+do
+    syst_files+="tmp/`basename $file .dat`_smear.dat "
+done
+
+echo "[NOMINAL VALUES] are in $nominal_file"
+echo "[SYST VALUES] are in $syst_files"
+
 
 paste $nominal_file $syst_files | sed "s/${commonCut}//g" > tmp/summary_syst_file.dat
 awk -f awk/syst.awk tmp/summary_syst_file.dat >tmp/summary_syst_file_2.dat
 sed -f sed/tex.sed tmp/summary_syst_file_2.dat >tmp/summary_syst_file.dat
+rm tmp/summary_syst_file_2.dat
 echo "% $syst_files">> tmp/summary_syst_file.dat
 
