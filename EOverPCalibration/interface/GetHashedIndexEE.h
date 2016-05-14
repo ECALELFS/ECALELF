05f31bb200261d20ce280f6adf049341138027fb
Shervin <shervin@cern.ch>
2016-05-14 09:23:39 +0200

    Revert "REVERT! This file does not compile on my laptot. Anyway need to understand why EEDetId cannot be used."
    This reverts commit 378e23b95ebff52ed92a74ee9ea900ceb9386e02.

---
+++ EOverPCalibration/interface/GetHashedIndexEE.h
@@ -0,0 +1,14 @@
+int GetHashedIndexEE(int, int, int);
+
+int GetIxFromHashedIndex(int);
+
+int GetIyFromHashedIndex(int);
+
+int GetZsideFromHashedIndex(int);
+
+// Essential values to get EE geometry
+static const int IX_MIN = 1;
+static const int IY_MIN = 1;
+static const int IX_MAX = 100;
+static const int IY_MAX = 100;
+static const int kEEhalf = 7324;
