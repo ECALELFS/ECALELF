934d45f0f870534329c03e35b181c00af777071c
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-05-19 12:15:28 +0200

    working on validation script

--- EcalAlCaRecoProducers/scripts/test.sh
+++ EcalAlCaRecoProducers/scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+GT=80X_dataRun2_Prompt_v8
 echo "============================================================"
 echo "Script for sandbox validation on a small amount of events   "
 
@@ -8,8 +8,12 @@ echo "[INFO] Creating a user directory in /tmp"
 local_dir=/tmp/$USER/sandboxValidation
 mkdir -p $local_dir
 
+myFile=`das_client --query="file dataset=/DoubleEG/Run2016B-v2/RAW" --limit 1 | grep store`
+
 echo "[INFO] Copy RAW-RECO file in local: taking file from EOS @CERN"
-xrdcp -v root://eoscms//eos/cms/store/group/alca_ecalcalib/sandbox/RAW-RECO.root $local_dir
+xrdcp -v  $local_dir
+
+cmsDriver.py myreco -s RAW2DIGI,RECO  --filein=$myFile --data --conditions=$ --era=Run2_2016  --scenario=pp --processName=reRECO --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs
 
 echo "[INFO] Running on 1000 events, output in $local_dir"
 cmsRun alcaSkimming.py files=file://$local_dir/RAW-RECO.root output=$local_dir/sandbox.root maxEvents=1000
