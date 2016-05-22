211df605776cc4b8da3cdde2ea901e96d99a9b82
Shervin <shervin@cern.ch>
2016-05-22 17:13:52 +0200

    Revert "Changes imported from CMSSW that need to be reviewed"
    This reverts commit f6d6cbed646ae7df1da3156308c02a1952e7fa25.

--- EcalAlCaRecoProducers/python/ALCARECOEcalRecalIsolElectron_Output_cff.py
+++ EcalAlCaRecoProducers/python/ALCARECOEcalRecalIsolElectron_Output_cff.py
@@ -4,13 +4,17 @@ import copy
 
 OutALCARECOEcalRecalElectron_noDrop=copy.deepcopy(OutALCARECOEcalCalElectron_noDrop)
 OutALCARECOEcalRecalElectron_noDrop.outputCommands+=cms.untracked.vstring(
-    'keep *_electronRecalibSCAssociator_*_*'
+    'keep *_electronRecalibSCAssociator_*_*',
+    'keep *_TriggerResults_*_*',
+    'keep *_*Uncalib*_*_*',
+#		'keep *_endcapRecalibSC_*_*',
+
     )
 
 OutALCARECOEcalRecalElectron=copy.deepcopy(OutALCARECOEcalRecalElectron_noDrop)
 OutALCARECOEcalRecalElectron.outputCommands.insert(0, "drop *")
 OutALCARECOEcalRecalElectron.outputCommands += cms.untracked.vstring(
-    'drop *_gedGsfElectron*_*_*',
+   # 'drop *_gedGsfElectron*_*_*',
     'drop *_*Unclean*_*_*',
     'drop *_*unclean*_*_*',
     'drop *_*_*Unclean*_*',
