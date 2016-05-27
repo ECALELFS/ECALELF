53f6df6827296d9426340007ce08764190f40f28
gfasanel <giuseppe.fasanella@cern.ch>
2016-05-27 19:28:21 +0200

    add script to quick test ntuple production

---
+++ EcalAlCaRecoProducers/scripts/local_run_test.sh
@@ -0,0 +1,10 @@
+#
+tagMC=config/reRecoTags/80X_mcRun2_asymptotic_2016_v3.py 
+file="root://xrootd.unl.edu//store/mc/RunIISpring16MiniAODv1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/00000/008099CA-5501-E611-9AAE-24BE05BDCEF1.root"
+#file="root://xrootd.unl.edu//store/mc/RunIISpring16MiniAODv1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3_ext1-v1/20000/0017320C-7BFC-E511-9B2D-0CC47A4C8E34.root"
+cmsRun python/alcaSkimming.py type=MINIAODNTUPLE files=file:$file maxEvents=10000 isCrab=0 tagFile=${tagMC} MC=1
+
+#data
+#tag_data=config/reRecoTags/80X_dataRun2_Prompt_v8.py
+#file="root://xrootd.unl.edu//store/data/Run2016B/DoubleEG/MINIAOD/PromptReco-v2/000/273/158/00000/0227DB1C-E719-E611-872C-02163E0141F9.root"
+#cmsRun python/alcaSkimming.py type=MINIAODNTUPLE files=file:$file maxEvents=10000 isCrab=0 tagFile=${tag_data} 
