11c922669fb5e732514d7dac63effe9c899841da
gfasanel <giuseppe.fasanella@cern.ch>
2016-05-29 11:14:13 +0200

    adding MC treatment in launch.sh

--- EcalAlCaRecoProducers/launch.sh
+++ EcalAlCaRecoProducers/launch.sh
@@ -8,7 +8,16 @@ jsonName=DCSONLY
 CHECK=--check
 
 #while [ "1" == "1" ];do sleep 1h; 
-./scripts/prodNtuples.sh `parseDatasetFile.sh alcareco_datasets.dat | grep RUN2016B | grep MINIAOD | head -1 ` --type=MINIAOD -s noSkim  --json=${json} --json_name=${jsonName} ${CHECK}
-./scripts/prodNtuples.sh `parseDatasetFile.sh alcareco_datasets.dat | grep RUN2016B | grep MINIAOD | head -2 | tail -1 ` --type=MINIAOD -s noSkim  --json=${json} --json_name=${jsonName} ${CHECK}
+#./scripts/prodNtuples.sh `parseDatasetFile.sh alcareco_datasets.dat | grep RUN2016B | grep MINIAOD | head -1 ` --type=MINIAOD -s noSkim  --json=${json} --json_name=${jsonName} ${CHECK}
+#./scripts/prodNtuples.sh `parseDatasetFile.sh alcareco_datasets.dat | grep RUN2016B | grep MINIAOD | head -2 | tail -1 ` --type=MINIAOD -s noSkim  --json=${json} --json_name=${jsonName} ${CHECK}
 
+
+option=--createOnly
+where=remoteGlidein
+tag_MC=config/reRecoTags/80X_mcRun2_asymptotic_2016_v3.py
+./scripts/prodNtuples.sh `parseDatasetFile.sh alcareco_datasets.dat | grep RunIISpring16-miniAODv1 | head -1` --type MINIAOD -t ${tag_MC} -s noSkim --json=${json} --json_name="RunII-2016" --scheduler=${where} ${option}
+
+if [[ $option == "--createOnly" ]]; then
+    crab -c prod_ntuples/MINIAODNTUPLE/80X_mcRun2_asymptotic_2016_v3/DYJets_amcatnlo-RunIISpring16-miniAODv1/allRange/RunII-2016/ -submit 1
+fi
 
