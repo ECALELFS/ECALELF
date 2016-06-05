40130aba278ce310fc981329fee927679cf25ce6
Shervin <shervin@cern.ch>
2016-06-05 21:23:22 +0200

    fixed prompt rereco and new ESMIP calibration

--- EcalAlCaRecoProducers/config/reRecoTags/test.py
+++ EcalAlCaRecoProducers/config/reRecoTags/test.py
@@ -3,8 +3,6 @@ import FWCore.ParameterSet.Config as cms
 #from CondCore.DBCommon.CondDBSetup_cfi import * #==> deprecated
 from CondCore.CondDB.CondDB_cfi import * 
 
-
-
 RerecoGlobalTag = cms.ESSource("PoolDBESSource",
                                CondDBSetup,
                                connect = cms.string('frontier://FrontierProd/CMS_COND_31X_GLOBALTAG'),
