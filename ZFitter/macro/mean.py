fe9ef4f337a253373bb0314b0df067b51e8d04dc
gfasanel <giuseppe.fasanella@cern.ch>
2016-06-25 16:28:54 +0200

    polishing of mean.py

--- ZFitter/macro/mean.py
+++ ZFitter/macro/mean.py
@@ -1,5 +1,6 @@
 import ROOT
-#####READ A FILE###################
+import math
+
 counter_barrel=0
 mean_barrel=0
 
@@ -20,5 +21,13 @@ with open('test/dato/10June_Golden_plus_diff_DCS/loose/invMass_SC_pho_regrCorr/t
             mean_endcap+=value
             counter_endcap+=1
 
-print "mean correction barrel is ", mean_barrel/counter_barrel
-print "mean correction endcap is ",mean_endcap/counter_endcap
+corr_EB=mean_barrel/counter_barrel
+corr_EE=mean_endcap/counter_endcap
+mass_corr_EBEB=corr_EB
+mass_corr_EBEE=math.sqrt(corr_EB*corr_EE)
+print "mean correction barrel is ", corr_EB
+print "mean correction endcap is ", corr_EE
+print "mass correction EBEB is ", mass_corr_EBEB
+print "mass correction EBEE is ", mass_corr_EBEE
+print("**********: EBEB -> 1 - corr: %.3lf" %(1 - mass_corr_EBEB))
+print("**********: EBEE -> 1 - corr: %.3lf" %(1 - mass_corr_EBEE))
