692d5ea85d1089762df81c8f7c5eaad25b159ef0
Shervin <shervin@cern.ch>
2016-09-11 23:13:04 +0200

    Updated json and alcaraw Run G

--- EcalAlCaRecoProducers/launchRereco.sh
+++ EcalAlCaRecoProducers/launchRereco.sh
@@ -8,16 +8,20 @@ jsonName=271036_276811-ICHEP
 json25ns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-279588_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt
 jsonName=271036_279588-Prompt
 
+#json25ns=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-279931_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt
+#jsonName=271036_279931-Prompt
+
+PERIOD=CAL_SEP2016
 #CHECK="--check"
 
-for tagfile in   config/reRecoTags/Cal_Sep2016_PS_{v1,v2}.py
+for tagfile in  config/reRecoTags/Cal_Sep2016_PS_{v1,v2}.py 
 do
 	echo
 #	./scripts/removeRereco.sh -t $tagfile -f alcarereco_datasets.dat
 #	./scripts/removeRereco.sh -t $tagfile -f ntuple_datasets.dat --json_name=$jsonName
 #	continue
 
-	for CHECK in  --check
+	for CHECK in '' --check
 	do
 		case $tagfile in 
 			*/Cal_Nov2015_PS_v1.py)
@@ -27,7 +31,7 @@ do
 			*)
 				echo 
 				#./scripts/RerecoQuick.sh -p RUN2016BCD -t $tagfile  --json=$json25ns --json_name="noJSON" ${CHECK} --alcarerecoOnly --weightsReco --singleEle
-				./scripts/RerecoQuick.sh -p CAL_SEP2016 -t $tagfile  --json=$json25ns --json_name="noJSON" ${CHECK} --alcarerecoOnly # --singleEle
+				./scripts/RerecoQuick.sh -p ${PERIOD} -t $tagfile  --json=$json25ns --json_name="noJSON" ${CHECK} --alcarerecoOnly # --singleEle
 				;;
 		esac
 	done
@@ -42,7 +46,7 @@ do
 				;;
 			*)
 			   #./scripts/RerecoQuick.sh -p RUN2016BCD -t $tagfile  --json=$json25ns --json_name=${jsonName} ${CHECK} --ntupleOnly --doEleIDTree --doExtraCalibTree --weightsReco --singleEle
-				./scripts/RerecoQuick.sh -p CAL_SEP2016 -t $tagfile --json=$json25ns --json_name=$jsonName --ntupleOnly  $CHECK 
+				./scripts/RerecoQuick.sh -p ${PERIOD} -t $tagfile --json=$json25ns --json_name=$jsonName --ntupleOnly  $CHECK 
 				;;
 		esac
 	done
