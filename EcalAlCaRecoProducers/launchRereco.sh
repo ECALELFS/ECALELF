4993bc1faf482f7e32e50a846333463ce455e601
Shervin <shervin@cern.ch>
2016-09-16 10:46:58 +0200

    New re-reco for validation of ADCtoGeV

--- EcalAlCaRecoProducers/launchRereco.sh
+++ EcalAlCaRecoProducers/launchRereco.sh
@@ -20,7 +20,7 @@ do
 #	./scripts/removeRereco.sh -t $tagfile -f ntuple_datasets.dat --json_name=$jsonName
 #	continue
 
-	for CHECK in '' --check
+	for CHECK in  --check
 	do
 		case $tagfile in 
 			*/Cal_Nov2015_PS_v1.py)
