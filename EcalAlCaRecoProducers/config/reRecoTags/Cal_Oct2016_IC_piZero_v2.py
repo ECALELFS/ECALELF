837ace0d222dc86c6e0c24a6b64bdee6b436a89d
Shervin <shervin@cern.ch>
2016-10-05 10:03:34 +0200

    New re-recoes with pi0 ICs

---
+++ EcalAlCaRecoProducers/config/reRecoTags/Cal_Oct2016_IC_piZero_v2.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+# Official GT for september re-reco
+# IC from GT + transport factor from piZero derived on 4fb-1 
+
+from CondCore.ESSources.CondDBESSource_cfi import * 
+#CondDBConnection.connect = cms.string( 'frontier://FrontierProd/CMS_CONDITIONS' )
+RerecoGlobalTag = GlobalTag.clone(
+    globaltag = cms.string('80X_dataRun2_2016SeptRepro_v3'),
+    toGet = cms.VPSet(
+        cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
+                 tag = cms.string("EcalIntercalibConstants_trans_2016vs2015_v1"),
+                 connect = cms.string("sqlite_file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_ECALCALIB/RunII-IC/Cal_Oct2016/pizero/tags/sqlite/EcalIntercalibConstants_trans_2016vs2015_v1.db"),
+                 )
+        ),
+    )
