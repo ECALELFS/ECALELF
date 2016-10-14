cc118b3709c89500b30735f67e738bac1c56dc37
Federico Ferri <federico.ferri@cern.ch>
2016-10-14 16:36:51 +0200

    use coherently _n as size_t, set by default MAXSIZE to 10000 (avoiding >10 GB virtual memory allocated with few categories), indent properly the functions

--- ZFitter/interface/Stats.hh
+++ ZFitter/interface/Stats.hh
@@ -8,7 +8,7 @@
 #include <algorithm>
 #include <TStopwatch.h>
 
-#define MAXSIZE 100000000
+#define MAXSIZE 10000
 
 /** \class stats stats stats
 	\brief class that provides statistical tools
@@ -21,6 +21,7 @@ public:
 	{
 		_values.reserve(maxSize);
 	}; ///< default constructor
+
 	stats(const std::vector<float>& v); ///< constructor starting from vector of values
 
 	~stats(void) {};
@@ -32,21 +33,25 @@ public:
 		_sum2=0.;
 	};
 
+
 	inline std::string name(void) const
 	{
 		return _name;
 	};
 
-	inline float n(void) const
+
+	inline size_t n(void) const
 	{
 		return _n;
 	}; ///< returns the number of values
+
 
 	inline float mean(void) const
 	{
 		if(_n == 0) return 0;
 		return _sum / _n;
 	}; ///< returns the mean
+
 
 	inline float stdDev(void) const
 	{
@@ -67,6 +72,7 @@ public:
 		return recursive_effective_mode(0, _n - 1, q, e);
 	};
 
+
 	float median(void) const
 	{
 		if(_n == 0) return 0;
@@ -74,6 +80,7 @@ public:
 		if(_n % 2 == 1) return _values[i];
 		else return 0.5 * (_values[i - 1] + _values[i]);
 	};
+
 
 	void add(float); ///< add one entry to the list of values
 	inline void sort(void)
@@ -85,24 +92,26 @@ public:
 		ts.Stop();
 		ts.Print();
 	};
-	friend std::ostream& operator<<(std::ostream& os, const stats s);
+
 
 	float operator[](size_t i)
 	{
 		return _values[i];
 	};
+
+
+	friend std::ostream& operator<<(std::ostream& os, const stats s);
+
 private:
 	std::string _name;
 	bool _isSorted;
 	double _sum;
 	double _sum2;
-	unsigned long long int _n;
+	size_t _n;
 
 	std::vector<float> _values;
 
-
 	float eff_sigma(std::vector<float> & v, float q = 0.68269);
-
 };
 
 class statsCollection
@@ -114,33 +123,46 @@ public:
 	{
 		_stats_coll.push_back(s);
 	};
+
+
 	stats_coll_t::iterator begin()
 	{
 		return _stats_coll.begin();
 	};
+
+
 	stats_coll_t::iterator end()
 	{
 		return _stats_coll.end();
 	};
 
-	size_t size(){
+
+	size_t size()
+        {
 		return _stats_coll.size();
 	};
 
-	void reset(){
+
+	void reset()
+        {
 		for(auto& s : _stats_coll){
 			s.clear();
 		}
 	};
 
+
 	size_t nEvents()
 	{
 		return _stats_coll[0].n();
 	};
+
+
 	stats& operator[](size_t i)
 	{
 		return _stats_coll[i];
 	}
+
+
 	void dump(std::string filename)
 	{
 		size_t nevts = nEvents();
