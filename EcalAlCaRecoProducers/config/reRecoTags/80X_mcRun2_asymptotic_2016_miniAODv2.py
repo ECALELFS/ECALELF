c059b964964b87080729ca809402218772225b1c
Shervin <shervin@cern.ch>
2016-11-03 12:06:33 +0100

    Few fixes already in newMaster

--- EcalAlCaRecoProducers/config/reRecoTags/80X_mcRun2_asymptotic_2016_miniAODv2.py
+++ EcalAlCaRecoProducers/config/reRecoTags/80X_mcRun2_asymptotic_2016_miniAODv2.py
@@ -1,9 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from CondCore.CondDB.CondDB_cfi import *
-# global tag for 80X MC production
-RerecoGlobalTag = cms.ESSource("PoolDBESSource",
-                               CondDB,
-                               connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
-                               globaltag = cms.string('80X_mcRun2_asymptotic_2016_miniAODv2'),
-                               )
+from CondCore.ESSources.CondDBESSource_cfi import * 
+#CondDBConnection.connect = cms.string( 'frontier://FrontierProd/CMS_CONDITIONS' )
+RerecoGlobalTag = GlobalTag.clone(
+    globaltag = cms.string('80X_mcRun2_asymptotic_2016_miniAODv2'),
+    )
+
