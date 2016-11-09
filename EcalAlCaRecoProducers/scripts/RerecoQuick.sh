fd3b9582815cdb4e1e36213b377da564a0e0ab96
Shervin <shervin@cern.ch>
2016-11-09 07:27:43 +0100

    Update list of re-recoes

--- EcalAlCaRecoProducers/scripts/RerecoQuick.sh
+++ EcalAlCaRecoProducers/scripts/RerecoQuick.sh
@@ -183,11 +183,10 @@ for dataset in $datasets
 	else
 		./scripts/prodNtuples.sh  -t ${TAGFILE} --type=ALCARERECO \
   			${JSONFILE} ${JSONNAME} \
-			--scheduler=$SCHEDULER --file_per_job=10   --doEleIDTree  --extraName=withEleID  ${DOEXTRACALIBTREE} ${EXTRAOPTION} ${EXTRAEXTRAOPTION} \
+			--scheduler=$SCHEDULER --file_per_job=10   --doEleIDTree --doExtraStudyTree --extraName=gainEleFix  ${DOEXTRACALIBTREE} ${EXTRAOPTION} ${EXTRAEXTRAOPTION} \
 			${TUTORIAL} $dataset 
 #--scheduler=$SCHEDULER --file_per_job=10  --doExtraCalibTree --doEleIDTree --doExtraStudyTree --extraName=withExtraStudy  ${DOEXTRACALIBTREE} ${EXTRAOPTION} ${EXTRAEXTRAOPTION} \
-
-
+		
 	fi
 	
 done
