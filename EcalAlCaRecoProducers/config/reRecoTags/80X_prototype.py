d092f9c9167e3c321197e1772e042971be2c5c30
gfasanel <giuseppe.fasanella@cern.ch>
2016-11-13 18:26:03 +0100

    working on crab3

--- EcalAlCaRecoProducers/config/reRecoTags/80X_prototype.py
+++ EcalAlCaRecoProducers/config/reRecoTags/80X_prototype.py
@@ -1,9 +1,36 @@
 import FWCore.ParameterSet.Config as cms
 
-from CondCore.CondDB.CondDB_cfi import *
+from CondCore.DBCommon.CondDBSetup_cfi import *
 
 RerecoGlobalTag = cms.ESSource("PoolDBESSource",
-                               CondDB,
-                               #connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
-                               globaltag = cms.string('80X_prototype'),
+                               CondDBSetup,
+                               connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                               globaltag = cms.string('80X_mcRun2_asymptotic_2016_miniAODv2'),
                                )
+
+##This does NOT work
+#import FWCore.ParameterSet.Config as cms
+#
+#from CondCore.CondDB.CondDB_cfi import * 
+#
+#RerecoGlobalTag = cms.ESSource("PoolDBESSource",
+#                               CondDB,
+#                               connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+#                               globaltag = cms.string('80X_mcRun2_asymptotic_2016_miniAODv2_v0'),
+#                               )
+
+
+#Exception Message:
+#python encountered the error: <type 'exceptions.ValueError'>
+#Duplicate insert of member connect
+#The original parameters are:
+#cms.ESSource("PoolDBESSource",
+#    DBParameters = cms.PSet(
+#        authenticationPath = cms.untracked.string(''),
+#        authenticationSystem = cms.untracked.int32(0),
+#        messageLevel = cms.untracked.int32(0),
+#        security = cms.untracked.string('')
+#    ),
+#    connect = cms.string('')
+#)
+
